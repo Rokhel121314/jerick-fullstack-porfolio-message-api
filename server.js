9e5e974e514c2dd46eefe90cf183f696ad8babda
Rokhel121314 <jerickddelacruz@gmail.com>
2023-02-03 13:56:13 +0800

    reverse cors

--- server.js
+++ server.js
@@ -10,15 +10,7 @@ const app = express();
 // MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://jerick-fullstack-portfolio.onrender.com/",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(messageRoutes);
 
 const PORT = process.env.PORT || 3001;
